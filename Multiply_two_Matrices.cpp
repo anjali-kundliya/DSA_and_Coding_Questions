cf351de50edcdb8f969b4063c0f729dee4b1a512
Anjali Kundliya <anjalikundliya@gmail.com>
2021-12-30 21:58:51 +0530

    multiply two given matrices

---
+++ Multiply_two_Matrices.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+public:
+    void Mutliply(vector<vector<int> >& matrixA, vector<vector<int>>&matrixB) {
+        // Code here
+        int N = matrixA.size();
+        vector<vector<int>> matrixC(N, vector<int> (N, 0));
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<N;j++)
+            {
+                matrixC[i][j] = 0;
+                for(int k = 0; k < N; k++)
+                {
+                    matrixC[i][j]+=matrixA[i][k]*matrixB[k][j];
+                }
+            }
+        }
+        matrixA = matrixC;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>> matrixA(n, vector<int>(n,0));
+		vector<vector<int>> matrixB(n, vector<int>(n,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrixA[i][j];
+			}
+		}
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrixB[i][j];
+			}
+		}
+		Solution ob;
+		ob.Mutliply(matrixA, matrixB);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				cout << matrixA[i][j] <<" ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}  // } Driver Code Ends
