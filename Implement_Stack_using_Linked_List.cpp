14fc29c85b08b84d266ff9a9ab9b7ee7aeb40684
Anjali Kundliya <anjalikundliya@gmail.com>
2022-01-03 23:01:00 +0530

    implementing stack using linked list

---
+++ Implement_Stack_using_Linked_List.cpp
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StackNode {
+    int data;
+    StackNode *next;
+    StackNode(int a) {
+        data = a;
+        next = NULL;
+    }
+};
+
+class MyStack {
+  private:
+    StackNode *top;
+
+  public:
+    void push(int);
+    int pop();
+    MyStack() { top = NULL; }
+};
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        MyStack *sq = new MyStack();
+
+        int Q;
+        cin >> Q;
+        while (Q--) {
+            int QueryType = 0;
+            cin >> QueryType;
+            if (QueryType == 1) {
+                int a;
+                cin >> a;
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
+
+//Function to push an integer into the stack.
+void MyStack ::push(int x)
+{
+    // Your Code
+    struct StackNode *ptr= new StackNode(x);
+    ptr->next=top;
+    top=ptr;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack ::pop()
+{
+    // Your Code
+    if(top==NULL)
+    return -1;
+
+    StackNode* temp=top;
+    top=top->next;
+
+    int data=temp->data;
+    free(temp);
+    return data;
+
+}
