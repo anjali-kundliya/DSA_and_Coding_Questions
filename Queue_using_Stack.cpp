7cbeb77ca5f2ffd2386393c63d23d84a7b7635ac
Anjali Kundliya <anjalikundliya@gmail.com>
2022-01-08 23:15:31 +0530

    implementing queue using stack

---
+++ Queue_using_Stack.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Queue {
+    stack<int> input, output;
+public:
+
+    void enqueue(int x) {
+        input.push(x);
+    }
+
+    int dequeue() {
+        if(input.empty()&&output.empty())
+        return -1;
+        if(output.empty()){
+        while(!input.empty())
+        {
+            output.push(input.top());
+            input.pop();
+        }
+        }
+        int v= output.top();
+        output.pop();
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Queue ob;
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            ob.enqueue(a);
+        }
+        else if(QueryType==2)
+        {
+            cout<<ob.dequeue()<<" ";
+
+        }
+        }
+    cout<<endl;
+    }
+}
+  // } Driver Code Ends
