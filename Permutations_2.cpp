eaf9b43fca0aecd1a76385bf91c89bf1f7d1b8c9
Anjali Kundliya <anjalikundliya@gmail.com>
2022-05-15 22:05:55 +0530

    permutation leetcode

---
+++ Permutations_2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(vector<int>& nums,int i)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        unordered_set<int> s;
+        for(int j=i;j<nums.size();j++)
+        {
+            if(s.find(nums[j])!=s.end())
+            {
+                continue;
+            }
+            s.insert(nums[j]);
+            swap(nums[i],nums[j]);
+            solve(nums,i+1);
+            swap(nums[i],nums[j]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        solve(nums,0);
+        return ans;
+    }
+};
