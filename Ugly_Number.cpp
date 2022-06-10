3696fd51c69f42174b1e0774306f8c5234d13349
Anjali Kundliya <anjalikundliya@gmail.com>
2022-06-10 16:51:10 +0530

    ugly number leetcode

---
+++ Ugly_Number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        bool ans=false;
+        if(n<=0){
+            return ans;
+        }
+        while(n!=1){
+            if(n%2==0){
+                n=n/2;
+            }
+            else if(n%3==0){
+                n=n/3;
+            }
+            else if(n%5==0){
+                n=n/5;
+            }
+            else{
+                break;
+            }
+        }
+        if(n==1){
+            ans=true;
+        }
+        return ans;
+    }
+};
