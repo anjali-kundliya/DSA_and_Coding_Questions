2344ed942088022c8c06f97fc38d55a66318ae53
Anjali Kundliya <anjalikundliya@gmail.com>
2022-06-13 13:27:21 +0530

    power of three leetcode

---
+++ Power_of_Three.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  public:
+    bool isPowerOfThree(int n) {
+        if( n == 0){
+            return false;
+        }
+
+        else if(n ==1){
+            return true;
+        }
+
+        else if(n % 3 == 0){
+            return isPowerOfThree(n/3);
+
+        }
+
+        else{
+            return false;
+        }
+    }
+};
