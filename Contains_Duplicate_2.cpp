28b0e5d62b2a50ba32ce1a745618bb5e8a0652a0
Anjali Kundliya <anjalikundliya@gmail.com>
2022-08-05 19:15:50 +0530

    contains duplicate leetcode

---
+++ Contains_Duplicate_2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(map.count(nums[i]) > 0)
+        {
+            if(abs(i - (map[nums[i]])) <= k)
+            {
+                return true;
+            }
+        }
+        map[nums[i]] = i;
+    }
+    return false;
+    }
+};
